7de5885cff2f5e3abe76fa537cb64bfcc072d852
ruihong123 <67431767+ruihong123@users.noreply.github.com>
2021-03-02 14:52:29 -0500

    Delete cmake-build-debug directory

--- cmake-build-debug/third_party/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ruihong/Projects/leveldb/third_party/googletest/googlemock
-# Build directory: /home/ruihong/Projects/leveldb/cmake-build-debug/third_party/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
