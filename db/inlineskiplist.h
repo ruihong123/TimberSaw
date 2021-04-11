4896748384ea01d442e9f525681dcb02ed21f1a5
ruihong123 <wang4996@purdue.edu>
2021-04-11 16:56:14 -0400

    skiplist debugging

--- db/inlineskiplist.h
+++ db/inlineskiplist.h
@@ -865,7 +865,8 @@ bool InlineSkipList<Comparator>::Insert(const char* key, Splice* splice,
   }
   assert(recompute_height <= max_height);
   if (recompute_height > 0) {
-    printf("head_ address is %p", head_->Next(0));
+    printf("head_ Next(0) address is %p", head_->Next(0));
+    printf("head_ Next(maxheight) address is %p", head_->Next(recompute_height));
 //    printf("splice print %p", splice->prev_[recompute_height]->Next(recompute_height));
     RecomputeSpliceLevels(key_decoded, splice, recompute_height);
 
