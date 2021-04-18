582e2a8dfae02297f1bfc338a405580f4ea9df18
ruihong123 <wang4996@purdue.edu>
2021-04-18 11:37:13 -0400

    memtable switching debug

--- db/db_impl.h
+++ db/db_impl.h
@@ -209,7 +209,7 @@ class DBImpl : public DB {
   Status bg_error_ GUARDED_BY(mutex_);
 
   CompactionStats stats_[config::kNumLevels] GUARDED_BY(mutex_);
-  std::atomic<size_t> memtable_counter;
+  std::atomic<size_t> memtable_counter = 0;
 };
 
 // Sanitize db options.  The caller should delete result.info_log if
