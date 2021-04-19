70f81bb58e81d51e527352cb131e37856b286220
ruihong123 <wang4996@purdue.edu>
2021-04-19 16:40:22 -0400

    read from RDMA

--- cmake-build-debug/third_party/benchmark/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ruihong/Projects/leveldb/third_party/benchmark/src
-# Build directory: /home/ruihong/Projects/leveldb/cmake-build-debug/third_party/benchmark/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
