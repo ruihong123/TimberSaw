0ff7acd3f108dbece8f93838434cf211801c4f4a
ruihong123 <wang4996@purdue.edu>
2021-05-17 11:22:24 -0400

    Debug the flushing and compaction proceeded together.

--- table/two_level_iterator.cc
+++ table/two_level_iterator.cc
@@ -72,12 +72,12 @@ void TwoLevelIterator::SkipEmptyDataBlocksForward() {
       return;
     }
     index_iter_.Next();
-//    DEBUG("Move to next block\n");
+    printf("Move to next block\n");
     InitDataBlock();
     if (data_iter_.iter() != nullptr) data_iter_.SeekToFirst();
   }
-//  DEBUG_arg("Move to next data, key is %s", data_iter_.key().ToString().c_str());
-//  DEBUG_arg("Iterator pointer is %p\n", this);
+  printf("Move to next data, key is %s", data_iter_.key().ToString().c_str());
+  printf("Iterator pointer is %p\n", this);
 }
 
 void TwoLevelIterator::SkipEmptyDataBlocksBackward() {
