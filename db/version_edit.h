38f8452df1bfd3877c2da14ecdf88d6526fb09ee
ruihong123 <wang4996@purdue.edu>
2021-05-17 21:14:44 -0400

    Debugging garbage collection.

--- db/version_edit.h
+++ db/version_edit.h
@@ -21,11 +21,13 @@ struct RemoteMemTableMetaData {
   RemoteMemTableMetaData() : refs(0), allowed_seeks(1 << 30) {}
   //TOTHINK: the garbage collection of the Remmote table is not triggered!
   ~RemoteMemTableMetaData() {
-    DEBUG("Destroying RemoteMemtableMetaData");
+    DEBUG("Destroying RemoteMemtableMetaData\n");
     if(Remote_blocks_deallocate(remote_data_mrs) &&
         Remote_blocks_deallocate(remote_dataindex_mrs) &&
     Remote_blocks_deallocate(remote_filter_mrs)){
       DEBUG("Remote blocks deleted successfully\n");
+    }else{
+      DEBUG("Remote memory collection failed\n");
     }
   }
   bool Remote_blocks_deallocate(std::map<int, ibv_mr*> map){
