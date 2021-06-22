38d4269410d4a060f8ad8a6ab94d257acce855b5
ruihong123 <wang4996@purdue.edu>
2021-06-21 22:23:12 -0400

    change configuration for multiple threaded flushing

--- include/leveldb/options.h
+++ include/leveldb/options.h
@@ -49,7 +49,7 @@ struct LEVELDB_EXPORT Options {
 
 
 
-  int max_background_compactions = 4;
+  int max_background_compactions = 6;
   bool usesubcompaction = true;
   // If true, the database will be created if it is missing.
   bool create_if_missing = false;
