dc9980142dfe16c6dc344908a32b3d3c5faecb6d
ruihong123 <wang4996@purdue.edu>
2021-07-19 21:39:57 -0400

    Table metadata read analysis

--- table/table.cc
+++ table/table.cc
@@ -219,10 +219,7 @@ Status Table::InternalGet(const ReadOptions& options, const Slice& k, void* arg,
     }
 #endif
   } else {
-#ifdef GETANALYSIS
-    TableCache::not_filtered.fetch_add(1);
-    auto start = std::chrono::high_resolution_clock::now();
-#endif
+
     Iterator* iiter = rep_->index_block->NewIterator(rep_->options.comparator);
 
     iiter->Seek(k);//binary search for block index
@@ -234,20 +231,23 @@ Status Table::InternalGet(const ReadOptions& options, const Slice& k, void* arg,
       BlockHandle handle;
 
       Iterator* block_iter = BlockReader(this, options, iiter->value());
-
+#ifdef GETANALYSIS
+      TableCache::not_filtered.fetch_add(1);
+      auto start = std::chrono::high_resolution_clock::now();
+#endif
       block_iter->Seek(k);
-
-      if (block_iter->Valid()) {
-        (*handle_result)(arg, block_iter->key(), block_iter->value());
-      }
-      s = block_iter->status();
-      delete block_iter;
 #ifdef GETANALYSIS
       auto stop = std::chrono::high_resolution_clock::now();
       auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
 //    std::printf("Block Reader time elapse is %zu\n",  duration.count());
       TableCache::BinarySearchTimeElapseSum.fetch_add(duration.count());
 #endif
+      if (block_iter->Valid()) {
+        (*handle_result)(arg, block_iter->key(), block_iter->value());
+      }
+      s = block_iter->status();
+      delete block_iter;
+
 #ifdef GETANALYSIS
       Saver* saver = reinterpret_cast<Saver*>(arg);
       if(saver->state == kFound){
