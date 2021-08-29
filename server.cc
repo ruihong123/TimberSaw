a2d0e7915427b3583211904c4c2b6e3efecd0aa6
ruihong123 <wang4996@purdue.edu>
2021-08-28 20:48:17 -0400

    add more print to debug the realease mode

--- server.cc
+++ server.cc
@@ -8,7 +8,7 @@ int main()
 {
 
   leveldb::Memory_Node_Keeper mn_keeper(true);
-  mn_keeper.SetBackgroundThreads(4, leveldb::ThreadPoolType::CompactionThreadPool);
+  mn_keeper.SetBackgroundThreads(12, leveldb::ThreadPoolType::CompactionThreadPool);
   mn_keeper.Server_to_Client_Communication();
 
   return 0;
