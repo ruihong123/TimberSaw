86098908f34cf4107a7f06ea1abb78c80310454d
ruihong123 <wang4996@purdue.edu>
2021-09-01 11:09:30 -0400

    Debug Read error for near data compaction

--- include/leveldb/options.h
+++ include/leveldb/options.h
@@ -120,7 +120,7 @@ struct LEVELDB_EXPORT Options {
   // Number of keys between restart points for delta encoding of keys.
   // This parameter can be changed dynamically.  Most clients should
   // leave this parameter alone.
-  int block_restart_interval = 16;
+  int block_restart_interval = 1;
 
   // Leveldb will write up to this amount of bytes to a file before
   // switching to a new one.
