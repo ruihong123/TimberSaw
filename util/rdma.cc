21be1bd3ce725e4bab5ab2ad497210520ee442e7
ruihong123 <wang4996@purdue.edu>
2021-09-01 20:22:55 -0400

    Polling details

--- util/rdma.cc
+++ util/rdma.cc
@@ -155,14 +155,14 @@ RDMA_Manager::~RDMA_Manager() {
   delete res;
 }
 bool RDMA_Manager::poll_reply_buffer(RDMA_Reply* rdma_reply) {
-  volatile bool* check_bit = &(rdma_reply->received);
-  while(!*check_bit){
-//    _mm_clflush();
-  asm volatile ("sfence\n" : : );
-  asm volatile ("lfence\n" : : );
-  asm volatile ("mfence\n" : : );
-  std::fprintf(stderr, "Polling reply buffer\r");
-  std::fflush(stderr);
+  volatile bool* check_byte = &(rdma_reply->received);
+  while(!*check_byte){
+    _mm_clflush(check_byte);
+    asm volatile ("sfence\n" : : );
+    asm volatile ("lfence\n" : : );
+    asm volatile ("mfence\n" : : );
+    std::fprintf(stderr, "Polling reply buffer\r");
+    std::fflush(stderr);
   }
   return true;
 }
