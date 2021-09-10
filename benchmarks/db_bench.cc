2e4d4afd9319a5336fe69b068133c8f1da5d1efb
ruihong123 <wang4996@purdue.edu>
2021-09-10 15:18:46 -0400

    fix the data block corruption on compute node

--- benchmarks/db_bench.cc
+++ benchmarks/db_bench.cc
@@ -76,7 +76,7 @@ static int FLAGS_reads = -1;
 static int FLAGS_threads = 1;
 
 // Size of each value
-static int FLAGS_value_size = 100;
+static int FLAGS_value_size = 400;
 // Size of each value
 static int FLAGS_key_size = 20;
 // Arrange to generate values that shrink to this fraction of
