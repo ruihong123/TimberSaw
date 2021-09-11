bd14f195735e9d254b8b617b241bfc36551b41fd
Prishita Kadam <55227184+prishitakadam@users.noreply.github.com>
2021-09-10 21:25:34 -0400

    Update memtable.h

--- db/memtable.h
+++ db/memtable.h
@@ -5,7 +5,7 @@
 #ifndef STORAGE_LEVELDB_DB_MEMTABLE_H_
 #define STORAGE_LEVELDB_DB_MEMTABLE_H_
 // #define MEMTABLE_SEQ_SIZE 153846 //Make the in memory buffer close to 64MB
-#define MEMTABLE_SEQ_SIZE 159783
+#define MEMTABLE_SEQ_SIZE 79892
 #include "db/dbformat.h"
 #include "db/inlineskiplist.h"
 #include <string>
