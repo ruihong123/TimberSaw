154b367eb07075efaa6b953e5c8b9e96d3aef1fb
ruihong123 <wang4996@purdue.edu>
2021-09-23 16:17:41 -0400

    sync option before running

--- db/db_impl.cc
+++ db/db_impl.cc
@@ -1663,11 +1663,11 @@ void DBImpl::client_message_polling_and_handling_thread(std::string q_id) {
       std::shared_lock<std::shared_mutex> l(rdma_mg->qp_cq_map_mutex);
 
       if (rdma_mg->res->qp_map.find(q_id) != rdma_mg->res->qp_map.end()){
-        qp = rdma_mg->res->qp_map.at(q_id);
-        assert(rdma_mg->res->qp_connection_info.find(q_id)!= rdma_mg->res->qp_connection_info.end());
-        l.unlock();
-        rdma_mg->modify_qp_to_reset(qp);
-        rdma_mg->connect_qp(qp, q_id);
+//        qp = rdma_mg->res->qp_map.at(q_id);
+//        assert(rdma_mg->res->qp_connection_info.find(q_id)!= rdma_mg->res->qp_connection_info.end());
+//        l.unlock();
+//        rdma_mg->modify_qp_to_reset(qp);
+//        rdma_mg->connect_qp(qp, q_id);
       }else{
         l.unlock();
         rdma_mg->Remote_Query_Pair_Connection(q_id);
