a32df4eab5419b3bec63a7c32596efa1f75ae959
ruihong123 <wang4996@purdue.edu>
2021-09-26 17:03:44 -0400

    sync option before running

--- util/ThreadPool.h
+++ util/ThreadPool.h
@@ -84,6 +84,7 @@ class ThreadPool{
     if (exit_all_threads_) {
       return;
     }
+    printf("schedule a work request!\n");
     StartBGThreads();
     // Add to priority queue
     queue_.push_back(BGItem());
