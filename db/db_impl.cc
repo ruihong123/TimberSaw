7fb771bb88137854c4f6960034a97be1720fa015
ruihong123 <wang4996@purdue.edu>
2021-09-30 14:51:43 -0400

    debug for valgrind

--- db/db_impl.cc
+++ db/db_impl.cc
@@ -2760,7 +2760,7 @@ Status DBImpl::Write(const WriteOptions& options, WriteBatch* updates) {
   std::printf("Real insert to memtable, time elapse is %zu\n",  duration.count());
   std::printf("total time, time elapse is %zu\n",  total_duration.count());
 #endif
-//  printf("A value has been inserted\n");
+  printf("A value has been inserted\n");
   return status;
 }
 
