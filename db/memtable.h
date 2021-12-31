b419d59fff71de4833f94c799c7f2e11a5d09e2f
ruihong123 <wang4996@purdue.edu>
2021-12-31 12:03:25 -0500

    Near data compaction communication. 2

--- db/memtable.h
+++ db/memtable.h
@@ -129,7 +129,7 @@ class MemTable {
   uint64_t Getlargest_seq_supposed() const{
     // in case that there is a unfull table flush, the largest seq will be different
     // from the one supposed
-    return largest_seq_supposed - MEMTABLE_SEQ_SIZE + seq_count;
+    return largest_seq_supposed;
   }
   uint64_t Getlargest_seq() const{
     // in case that there is a unfull table flush, the largest seq will be different
