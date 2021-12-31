d574c48e2ef8d7e0dc067c35c20cb8ee77ea37bb
ruihong123 <wang4996@purdue.edu>
2021-12-31 14:35:31 -0500

    Near data compaction communication. 2

--- db/memtable.h
+++ db/memtable.h
@@ -66,15 +66,15 @@ class MemTable {
   void Unref() {
     refs_.fetch_sub(1);
     assert(refs_ >= 0);
-#ifndef NDEBUG
-    if (full_table_flush){
-      assert(seq_count.load() == MEMTABLE_SEQ_SIZE);
-    }
-
-#endif
 
     if (refs_ <= 0) {
       // TODO: THis assertion may changed in the future
+#ifndef NDEBUG
+      if (full_table_flush){
+        assert(seq_count.load() == MEMTABLE_SEQ_SIZE);
+      }
+
+#endif
 
       delete this;
     }
