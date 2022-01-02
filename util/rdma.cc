d3e7fd8d7d7e5278b06010785dcf4fabb111f705
ruihong123 <wang4996@purdue.edu>
2022-01-02 18:37:46 -0500

    Near data compaction communication. 2

--- util/rdma.cc
+++ util/rdma.cc
@@ -2461,7 +2461,8 @@ void RDMA_Manager::Allocate_Local_RDMA_Slot(ibv_mr& mr_input,
 }
 void RDMA_Manager::BatchGarbageCollection(uint64_t* ptr, size_t size) {
   for (int i = 0; i < size/ sizeof(uint64_t); ++i) {
-    Deallocate_Local_RDMA_Slot((void*)ptr[i], "FlushBuffer");
+    bool result = Deallocate_Local_RDMA_Slot((void*)ptr[i], "FlushBuffer");
+    assert(result);
   }
 }
 
