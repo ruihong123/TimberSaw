0725e868c4c6907d3528af58aa14e70efa7b98ed
ruihong123 <wang4996@purdue.edu>
2022-01-03 00:12:32 -0500

    Near data compaction communication. 2

--- table/block.cc
+++ table/block.cc
@@ -69,6 +69,7 @@ Iterator* Block::NewIterator(const Comparator* comparator) {
     return NewErrorIterator(Status::Corruption("bad block contents"));
   }
   const uint32_t num_restarts = NumRestarts();
+  assert(num_restarts<= 64*1024*1024);
   if (num_restarts == 0) {
     return NewEmptyIterator();
   } else {
