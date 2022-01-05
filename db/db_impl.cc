0caa5a14a08e06264bd7d28ae40ecde0a6f7589c
ruihong123 <wang4996@purdue.edu>
2022-01-04 23:08:28 -0500

    Near data compaction communication. 2

--- db/db_impl.cc
+++ db/db_impl.cc
@@ -2029,7 +2029,9 @@ void DBImpl::client_message_polling_and_handling_thread(std::string q_id) {
           byte_len = wc[0].byte_len;
           cv_temp.notify_all();
           lck.unlock();
-          while (imm_data != 0 || byte_len != 0 ){}
+          while (imm_data != 0 || byte_len != 0 ){
+            cv_temp.notify_one();
+          }
           rdma_mg->post_receive<RDMA_Request>(&recv_mr[buffer_counter], "main");
           // increase the buffer index
           if (buffer_counter== R_SIZE-1 ){
