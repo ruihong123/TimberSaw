a55ea739b77e61c313cd4be6ced6e6a19134828c
ruihong123 <wang4996@purdue.edu>
2022-01-11 18:22:43 -0500

    debug for the edit merger

--- memory_node/memory_node_keeper.cpp
+++ memory_node/memory_node_keeper.cpp
@@ -347,8 +347,7 @@ void Memory_Node_Keeper::UnpinSSTables_RPC(VersionEdit_Merger* edit_merger,
   uint64_t* arr_ptr = (uint64_t*)send_mr_large.addr;
   uint32_t index = 0;
   for(auto iter : *edit_merger->GetNewFiles()){
-    if (iter.second->number == 4)
-      printf("check here");
+    DEBUG_arg("Unpin file number is %lu, id 1", iter.second->number);
     arr_ptr[index] = iter.second->number;
     index++;
   }
@@ -410,8 +409,7 @@ void Memory_Node_Keeper::UnpinSSTables_RPC(std::list<uint64_t>* merged_file_numb
   uint64_t* arr_ptr = (uint64_t*)send_mr_large.addr;
   uint32_t index = 0;
   for(auto iter : *merged_file_number){
-    if (iter == 4)
-      printf("check here");
+    DEBUG_arg("Unpin file number is %lu, id 2", iter);
     arr_ptr[index] = iter;
     index++;
   }
