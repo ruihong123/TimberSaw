63a04fdae16716c104ce5ab26fb69349cdd34bfb
ruihong123 <wang4996@purdue.edu>
2022-01-18 13:52:33 -0500

    Reduce the memory allocator overhead during read

--- benchmarks/db_bench.cc
+++ benchmarks/db_bench.cc
@@ -783,7 +783,7 @@ class Benchmark {
     delete[] arg;
     db_->WaitforAllbgtasks();
     if (method == &Benchmark::WriteRandom)
-      sleep(45); // wait for the last sstable disgestion.
+      sleep(32); // wait for the last sstable disgestion.
   }
 
   void Crc32c(ThreadState* thread) {
