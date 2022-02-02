e2815a7340145cffc04f17b7db0587d05b75d8d8
ruihong123 <wang4996@purdue.edu>
2022-02-01 20:23:44 -0500

    bash script test

--- script/multi_node_bench.sh
+++ script/multi_node_bench.sh
@@ -65,7 +65,7 @@ function run_bench() {
   while [ $n -lt $nshard ]
   do
     echo "Set up the ${compute_shard[n]}"
-    ssh -o StrictHostKeyChecking=no ${compute_shard[n]} "screen -d -m pwd && cd /users/Ruihong && sudo apt install numactl " &
+    ssh -o StrictHostKeyChecking=no ${compute_shard[n]} "screen -d -m pwd && cd /users/Ruihong/TimberSaw/build && git pull && make db_bench -j 32 && make Server -j 32 && sudo apt install numactl " &
     #"screen -d -m pwd && cd /users/Ruihong && git clone --recurse-submodules $github_repo && cd TimberSaw/ && mkdir build &&  cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && sudo apt-get install -y libnuma-dev  && make db_bench -j 32 && make Server -j 32" &
     n=$((n+1))
     sleep 1
