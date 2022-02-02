7989cfb519cef550d2784c08879ed9ac28051b26
ruihong123 <wang4996@purdue.edu>
2022-02-01 22:53:01 -0500

    bash script test

--- script/multi_node_bench.sh
+++ script/multi_node_bench.sh
@@ -61,38 +61,38 @@ function run_bench() {
   echo memory shards are ${memory_shard[@]}
   echo communication ports are ${communication_port[@]}
   #test for dowload and compile the codes
-  n=0
-  while [ $n -lt $nshard ]
-  do
-    echo "Set up the ${compute_shard[n]}"
-    ssh -o StrictHostKeyChecking=no ${compute_shard[n]} "screen -d -m pwd && cd /users/Ruihong/TimberSaw/build && git checkout $gitbranch && git pull && make db_bench -j 32 && make Server -j 32 && sudo apt install numactl " &
-#    ssh -o StrictHostKeyChecking=no ${compute_shard[n]}  "screen -d -m pwd && cd /users/Ruihong && git clone --recurse-submodules $github_repo && cd TimberSaw/ && mkdir build &&  cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && sudo apt-get install -y libnuma-dev  && make db_bench -j 32 && make Server -j 32" &
-    n=$((n+1))
-    sleep 1
-  done
-
-##Set up memory nodes
-#  n=0
-#  while [ $n -lt $nshard ]
-#  do
-#    echo "Set up the ${memory_shard[n]}"
-#    ssh -o StrictHostKeyChecking=no ${memory_shard[n]} "screen -d -m pwd && cd /users/Ruihong/TimberSaw/build &&
-#    numactl --cpunodebind=${numa_node[n%${#numa_node[@]}]} --localalloc ./Server ${communication_port[n]} 6 " &
-#    #
-#    n=$((n+1))
-#
-#  done
-#  sleep 70
-##Set up compute nodes and run the benchmark at the same time
 #  n=0
 #  while [ $n -lt $nshard ]
 #  do
 #    echo "Set up the ${compute_shard[n]}"
-#    ssh -o StrictHostKeyChecking=no ${compute_shard[n]} "screen -d -m pwd && cd /users/Ruihong/TimberSaw/build &&
-#    numactl --cpunodebind=all --localalloc ./db_bench --benchmarks=fillrandomshard,readrandomshard,readrandomshard --threads=16 --value_size=400 --block_size=8192 --num=312500 --bloom_bits=10 --cache_size=67108864 --IP=${memory_shard[n]} --tcpport=${communication_port[n]} --shardnum=${n} > result.txt" &
-#    #
+#    ssh -o StrictHostKeyChecking=no ${compute_shard[n]} "screen -d -m pwd && cd /users/Ruihong/TimberSaw/build && git checkout $gitbranch && git pull && make db_bench -j 32 && make Server -j 32 && sudo apt install numactl " &
+##    ssh -o StrictHostKeyChecking=no ${compute_shard[n]}  "screen -d -m pwd && cd /users/Ruihong && git clone --recurse-submodules $github_repo && cd TimberSaw/ && mkdir build &&  cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && sudo apt-get install -y libnuma-dev  && make db_bench -j 32 && make Server -j 32" &
 #    n=$((n+1))
-##    sleep 1
+#    sleep 1
 #  done
+
+#Set up memory nodes
+  n=0
+  while [ $n -lt $nshard ]
+  do
+    echo "Set up the ${memory_shard[n]}"
+    ssh -o StrictHostKeyChecking=no ${memory_shard[n]} "screen -d -m pwd && cd /users/Ruihong/TimberSaw/build &&
+    numactl --cpunodebind=${numa_node[n%${#numa_node[@]}]} --localalloc ./Server ${communication_port[n]} 6 " &
+    #
+    n=$((n+1))
+
+  done
+  sleep 70
+#Set up compute nodes and run the benchmark at the same time
+  n=0
+  while [ $n -lt $nshard ]
+  do
+    echo "Set up the ${compute_shard[n]}"
+    ssh -o StrictHostKeyChecking=no ${compute_shard[n]} "screen -d -m pwd && cd /users/Ruihong/TimberSaw/build &&
+    numactl --cpunodebind=all --localalloc ./db_bench --benchmarks=fillrandomshard,readrandomshard,readrandomshard --threads=16 --value_size=400 --block_size=8192 --num=312500 --bloom_bits=10 --cache_size=67108864 --IP=${memory_shard[n]} --tcpport=${communication_port[n]} --shardnum=${n} > result.txt" &
+    #
+    n=$((n+1))
+#    sleep 1
+  done
 	}
 	run_bench
