3d9a4a9d79df984a538c5d0920d5236e775abd12
ruihong123 <wang4996@purdue.edu>
2022-02-01 20:31:27 -0500

    bash script test

--- script/multi_node_bench.sh
+++ script/multi_node_bench.sh
@@ -61,27 +61,28 @@ function run_bench() {
   echo memory shards are ${memory_shard[@]}
   echo communication ports are ${communication_port[@]}
   #test for dowload and compile the codes
-  n=1
+  n=0
   while [ $n -lt $nshard ]
   do
     echo "Set up the ${compute_shard[n]}"
-    ssh -o StrictHostKeyChecking=no ${compute_shard[n]} "screen -d -m pwd && cd /users/Ruihong/TimberSaw/build && git pull && make db_bench -j 32 && make Server -j 32 && sudo apt install numactl " &
-    # command for initial clone "screen -d -m pwd && cd /users/Ruihong && git clone --recurse-submodules $github_repo && cd TimberSaw/ && mkdir build &&  cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && sudo apt-get install -y libnuma-dev  && make db_bench -j 32 && make Server -j 32" &
+#    ssh -o StrictHostKeyChecking=no ${compute_shard[n]} "screen -d -m pwd && cd /users/Ruihong/TimberSaw/build && git pull && make db_bench -j 32 && make Server -j 32 && sudo apt install numactl " &
+    ssh -o StrictHostKeyChecking=no ${compute_shard[n]} command for initial clone "screen -d -m pwd && cd /users/Ruihong && git clone --recurse-submodules $github_repo && cd TimberSaw/ && mkdir build &&  cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && sudo apt-get install -y libnuma-dev  && make db_bench -j 32 && make Server -j 32" &
     n=$((n+1))
     sleep 1
   done
 
-#Set up servers
-#  n=1
+#Set up memory nodes
+#  n=0
 #  while [ $n -lt $nshard ]
 #  do
-#    echo "Set up the ${compute_shard[n]}"
+#    echo "Set up the ${memory_shard[n]}"
 #    ssh -o StrictHostKeyChecking=no ${memory_shard[n]} "screen -d -m pwd && cd /users/Ruihong/TimberSaw/build &&
-#    numactl " &
+#    numactl --cpunodebind=${numa_node[n%${#numa_node[@]}]} --localalloc ./Server ${communication_port[n]} 6 " &
 #    #
 #    n=$((n+1))
 #    sleep 1
 #  done
+#Set up compute nodes and run the benchmark
 
 	}
 	run_bench
