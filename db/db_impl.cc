fe2142829f426173632c214c9f06e4c0b78c496e
ruihong123 <wang4996@purdue.edu>
2022-02-18 00:50:22 -0500

    roll back the superversion mutex. The problem should be the too much level0 SSTables. SO I increase the delay time

--- db/db_impl.cc
+++ db/db_impl.cc
@@ -1652,11 +1652,11 @@ SuperVersion* DBImpl::GetThreadLocalSuperVersion() {
       // NOTE: underlying resources held by superversion (sst files) might
       // not be released until the next background job.
       sv->Cleanup();
-//      std::unique_lock<std::mutex> lck(superversion_memlist_mtx);
+      std::unique_lock<std::mutex> lck(superversion_memlist_mtx);
       sv = super_version->Ref();
 
     } else {
-//      std::unique_lock<std::mutex> lck(superversion_memlist_mtx);
+      std::unique_lock<std::mutex> lck(superversion_memlist_mtx);
       sv = super_version->Ref();
     }
 
@@ -3345,7 +3345,7 @@ Status DBImpl::PickupTableToWrite(bool force, uint64_t seq_num, MemTable*& mem_r
       }
 //      imm_mtx.unlock();
     } else if(level0_filenum > config::kL0_SlowdownWritesTrigger && !delayed){
-      env_->SleepForMicroseconds(5);
+      env_->SleepForMicroseconds(2000);
       delayed = true;
     }else{
       std::unique_lock<std::mutex> l(superversion_memlist_mtx);
