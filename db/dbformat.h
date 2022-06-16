a763c0fe5554ca497ade60aaa36e97a0da3c7e68
ruihong123 <wang4996@purdue.edu>
2022-06-16 19:42:29 -0400

    2M2C

--- db/dbformat.h
+++ db/dbformat.h
@@ -28,7 +28,7 @@ static const int MaxImmuNumPerFlush = 1;
 // Immutable flushing will be triggered when hit this number
 static const int Immutable_FlushTrigger = 1;
 // Maximum number of unflushed immutable files 10 in 1-1, 16 in M-M
-static const int Immutable_StopWritesTrigger = 15;
+static const int Immutable_StopWritesTrigger = 7;
 // Level-0 compaction is started when we hit this many files.
 static const int kL0_CompactionTrigger = 1;
 
