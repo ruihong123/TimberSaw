2bcd769833bab1a9ffecc015e29aa3ca340905a2
ruihong123 <wang4996@purdue.edu>
2022-06-15 22:41:07 -0400

    change preallocation, change the benchmark file

--- benchmarks/db_bench.cc
+++ benchmarks/db_bench.cc
@@ -834,7 +834,7 @@ class Benchmark {
     }
     delete[] arg;
     db_->WaitforAllbgtasks(false);
-    if (method == &Benchmark::WriteRandom)
+    if (method == &Benchmark::WriteRandom || method == &Benchmark::WriteRandomSharded)
       sleep(35); // wait for the last sstable disgestion.
 
     if (method == &Benchmark::ReadRandom)
