ef97546f709667a44580c3b000866f5477bb2487
ruihong123 <wang4996@purdue.edu>
2022-06-17 19:51:13 -0400

    8M8C and add a script

--- script/cloudlab_install.sh
+++ script/cloudlab_install.sh
@@ -60,7 +60,7 @@ function run_bench() {
   echo compute shards are ${compute_shard[@]}
   echo memory shards are ${memory_shard[@]}
   echo communication ports are ${communication_port[@]}
-  test for dowload and compile the codes
+#  test for download and compile the codes
   n=0
   while [ $n -lt $nshard ]
   do
@@ -80,19 +80,19 @@ function run_bench() {
 #  do
 #    echo "Set up the ${memory_shard[n]}"
 #    ssh -o StrictHostKeyChecking=no ${memory_shard[n]} "screen -d -m pwd && cd /users/Ruihong/TimberSaw/build &&
-#    numactl --cpunodebind=all --localalloc ./Server ${communication_port[n]} 6 " &
+#    numactl --cpunodebind=all --localalloc ./Server ${communication_port[n]} 56 $n" &
 #    #${numa_node[n%${#numa_node[@]}]}
 #    n=$((n+1))
 #
 #  done
-#  sleep 40
+#  sleep 10
 ##Set up compute nodes and run the benchmark at the same time
 #  n=0
 #  while [ $n -lt $nshard ]
 #  do
 #    echo "Set up the ${compute_shard[n]}"
 #    ssh -o StrictHostKeyChecking=no ${compute_shard[n]} "screen -d -m pwd && cd /users/Ruihong/TimberSaw/build &&
-#    numactl --cpunodebind=all --localalloc ./db_bench --benchmarks=fillrandomshard,readrandomshard,readrandomshard --threads=16 --value_size=400 --block_size=8192 --num=312500 --bloom_bits=10 --cache_size=67108864 --IP=${memory_shard[n]} --tcpport=${communication_port[n]} --shardnum=${n} > result_1M8C.txt" &
+#    numactl --cpunodebind=all --localalloc ./db_bench --benchmarks=fillrandomshard,readrandomshard,readrandomshard --threads=16 --value_size=400 --block_size=8192 --num=3125000 --bloom_bits=10 --cache_size=67108864 --readwritepercent=50 --compute_node_id=$i > result_8M8C.txt" &
 #    #
 #    n=$((n+1))
 ##    sleep 1
