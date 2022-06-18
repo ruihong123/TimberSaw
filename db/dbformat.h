43fa9641bb00c26ce511652bc2c95a8814e824b0
ruihong123 <wang4996@purdue.edu>
2022-06-17 21:26:07 -0400

    8M8C and add a script. change the kL0_StopWritesTrigger with the shard number

--- db/dbformat.h
+++ db/dbformat.h
@@ -34,10 +34,14 @@ static const int Immutable_StopWritesTrigger = 1;
 static const int kL0_CompactionTrigger = 1;
 
 // Soft limit on number of level-0 files.  We slow down writes at this point.
-static const int kL0_SlowdownWritesTrigger = 20;
+// in M-M the total number of kL0_SlowdownWritesTrigger accross shard is 24
+// in 1-1 this value is 20
+static const int kL0_SlowdownWritesTrigger = 3;
 
 // Maximum number of level-0 files.  We stop writes at this point.
-static const int kL0_StopWritesTrigger = 36;
+// in M-M the total number of kL0_SlowdownWritesTrigger accross shard is 40
+// in 1-1 this value is 36
+static const int kL0_StopWritesTrigger = 5;
 // We  can set it as 48*64 Mega byte for the first level, then there will
 // be two levels after the random file benchmark. 1-1 256.0
 // M-M still 256. ahigher number of this value can result in low write performance
