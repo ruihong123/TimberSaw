be3316f16e3cd384ce0064109652703c93e89e30
ruihong123 <wang4996@purdue.edu>
2022-06-28 13:09:30 -0400

    bigdata setup 4 shards

--- db/dbformat.h
+++ db/dbformat.h
@@ -30,7 +30,7 @@ static const int Immutable_FlushTrigger = 1;
 // Maximum number of unflushed immutable files 10 in 1-1,
 // 16 totally across shards in M-M (including memtable)
 // with 8 fixed shard per compute node this equals 2 or 1
-static const int Immutable_StopWritesTrigger = 1; // should be 15
+static const int Immutable_StopWritesTrigger = 3; // should be 15
 // Level-0 compaction is started when we hit this many files.
 static const int kL0_CompactionTrigger = 1;
 
@@ -38,13 +38,13 @@ static const int kL0_CompactionTrigger = 1;
 // in M-M the total number of kL0_SlowdownWritesTrigger accross shard is 24
 // in 1-1 this value is 20
 // with 8 fixed shard per compute node this equals 2
-static const int kL0_SlowdownWritesTrigger = 2;
+static const int kL0_SlowdownWritesTrigger = 4;
 
 // Maximum number of level-0 files.  We stop writes at this point.
 // in M-M the total number of kL0_SlowdownWritesTrigger accross shard is 40
 // in 1-1 this value is 36
 // with 8 fixed shard per compute node this equals 4
-static const int kL0_StopWritesTrigger = 4;
+static const int kL0_StopWritesTrigger = 8;
 // We  can set it as 48*64 Mega byte for the first level, then there will
 // be two levels after the random file benchmark. 1-1 256.0
 // M-M still 256. ahigher number of this value can result in low write performance
