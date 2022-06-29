78a3f6fba933e792c7c75437c67d7d077d5f3abe
ruihong123 <wang4996@purdue.edu>
2022-06-29 13:04:39 -0400

    cloudlab setup mode 3

--- util/rdma.h
+++ util/rdma.h
@@ -70,7 +70,7 @@ enum Chunk_type {Message, Version_edit, IndexChunk, FilterChunk, FlushBuffer, Da
 static const char * EnumStrings[] = { "Message", "Version_edit",
       "IndexChunk", "FilterChunk", "FlushBuffer", "Default" };
 
-static char config_file_name[100] = "../connection_bigdata.conf";
+static char config_file_name[100] = "../connection_cloudlab.conf";
 
 struct config_t {
   const char* dev_name;    /* IB device name */
