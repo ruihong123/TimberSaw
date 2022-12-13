af11d74033b1aac8a15efac70548883819e2bac6
ruihong123 <wang4996@purdue.edu>
2022-12-13 16:21:28 -0500

    bigdata 800M retry;

--- include/TimberSaw/options.h
+++ include/TimberSaw/options.h
@@ -71,8 +71,8 @@ struct TimberSaw_EXPORT Options {
 
 
 
-  int max_background_compactions = 6;//
-  int MaxSubcompaction = 6; // 1-1 setup is 12; M-M  12 as well
+  int max_background_compactions = 3;//
+  int MaxSubcompaction = 3; // 1-1 setup is 12; M-M  12 as well
   bool usesubcompaction = true;
   // If true, the database will be created if it is missing.
   bool create_if_missing = true;
