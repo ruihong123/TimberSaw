2c8e4a0ca7f8f654a9918b6b8e9aeb40ee0c190e
wang4996 <wang4996@purdue.edu>
2022-12-20 17:43:08 -0500

    roll back the concurrent

--- db/db_impl.cc
+++ db/db_impl.cc
@@ -85,16 +85,16 @@ Options SanitizeOptions(const std::string& dbname,
 //  ClipToRange(&result.write_buffer_size, 64 << 10, 1 << 30);
 //  ClipToRange(&result.max_file_size, 1 << 20, 1 << 30);
 //  ClipToRange(&result.block_size, 1 << 10, 4 << 20);
-  if (result.info_log == nullptr) {
-    // Open a log file in the same directory as the db
-    src.env->CreateDir(dbname);  // In case it does not exist
-    src.env->RenameFile(InfoLogFileName(dbname), OldInfoLogFileName(dbname));
-    Status s = src.env->NewLogger(InfoLogFileName(dbname), &result.info_log);
-    if (!s.ok()) {
-      // No place suitable for logging
-      result.info_log = nullptr;
-    }
-  }
+//  if (result.info_log == nullptr) {
+//    // Open a log file in the same directory as the db
+//    src.env->CreateDir(dbname);  // In case it does not exist
+//    src.env->RenameFile(InfoLogFileName(dbname), OldInfoLogFileName(dbname));
+//    Status s = src.env->NewLogger(InfoLogFileName(dbname), &result.info_log);
+//    if (!s.ok()) {
+//      // No place suitable for logging
+//      result.info_log = nullptr;
+//    }
+//  }
 //  if (result.block_cache == nullptr) {
 //    result.block_cache = NewLRUCache(64 << 20);
 //  }
