f65c712c443d85fa05d0f81ada1dea90c7786d4f
wang4996 <wang4996@purdue.edu>
2022-12-19 21:21:24 -0500

    cloud lab tuning

--- include/TimberSaw/options.h
+++ include/TimberSaw/options.h
@@ -23,7 +23,7 @@ class Snapshot;
 // default 8 8  1
 #define RDMA_WRITE_BLOCK  (8*1024*1024)
 #define INDEX_BLOCK  (8*1024*1024)
-#define FILTER_BLOCK  (1*1024*1024)
+#define FILTER_BLOCK  (256*1024)
 //static size_t RDMA_WRITE_BLOCK = 1*1024*1024;
 // DB contents are stored in a set of blocks, each of which holds a
 // sequence of key,value pairs.  Each block may be compressed before
