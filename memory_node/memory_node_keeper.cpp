0f049b95020ad38ec3dd3092fa1caebc1da2ffd1
wang4996 <wang4996@purdue.edu>
2023-01-28 16:31:28 -0500

    more complicated strategy.

--- memory_node/memory_node_keeper.cpp
+++ memory_node/memory_node_keeper.cpp
@@ -1414,9 +1414,9 @@ Status Memory_Node_Keeper::InstallCompactionResultsToComputePreparation(
 
     // directly start the heart beat from the remote memory side, when all the connection to
     // the compute nodes are ready.
-    if (rdma_mg->connection_counter.load() == rdma_mg->compute_nodes.size()){
-      create_cpu_util_heart_beater_sender();
-    }
+//    if (rdma_mg->connection_counter.load() == rdma_mg->compute_nodes.size()){
+//      create_cpu_util_heart_beater_sender();
+//    }
 
     int buffer_position = 0;
     int miss_poll_counter = 0;
@@ -1555,7 +1555,7 @@ Status Memory_Node_Keeper::InstallCompactionResultsToComputePreparation(
         rdma_mg->post_receive<RDMA_Request>(&recv_mr[buffer_position],
                                             compute_node_id,
                                             client_ip);
-        printf("CPU utilization is %Lf\n", TimberSaw::Memory_Node_Keeper::rdma_mg->rpter.getCurrentValue());
+        printf("CPU utilization is %f\n", TimberSaw::Memory_Node_Keeper::rdma_mg->rpter.getCurrentValue());
 
       } else {
         printf("corrupt message from client. %d\n", receive_msg_buf->command);
