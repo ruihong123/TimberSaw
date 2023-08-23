428a5410ad064724d198a42b6d102f3b5e728d03
ruihong123 <wang4996@purdue.edu>
2023-08-22 22:59:19 -0400

    fix bug for checkpointing

--- memory_node/memory_node_keeper.cpp
+++ memory_node/memory_node_keeper.cpp
@@ -448,11 +448,11 @@ void Memory_Node_Keeper::UnpinSSTables_RPC(VersionEdit_Merger* edit_merger,
   asm volatile ("sfence\n" : : );
   asm volatile ("lfence\n" : : );
   asm volatile ("mfence\n" : : );
-  rdma_mg->post_send<RDMA_Request>(&send_mr, 0, client_ip);
+  rdma_mg->post_send<RDMA_Request>(&send_mr, target_node_id, client_ip);
   //TODO: Think of a better way to avoid deadlock and guarantee the same
   // sequence of verision edit between compute node and memory server.
   ibv_wc wc[2] = {};
-  if (rdma_mg->poll_completion(wc, 1, client_ip, true, 0)){
+  if (rdma_mg->poll_completion(wc, 1, client_ip, true, target_node_id)){
     fprintf(stderr, "failed to poll send for remote memory register\n");
     return;
   }
@@ -474,7 +474,7 @@ void Memory_Node_Keeper::UnpinSSTables_RPC(VersionEdit_Merger* edit_merger,
   rdma_mg->RDMA_Write(receive_pointer->buffer_large,
                       receive_pointer->rkey_large, &send_mr_large,
                       index * sizeof(uint64_t) + 1, client_ip,
-                      IBV_SEND_SIGNALED, 1, 0);
+                      IBV_SEND_SIGNALED, 1, target_node_id);
   rdma_mg->Deallocate_Local_RDMA_Slot(send_mr_large.addr,Version_edit);
   rdma_mg->Deallocate_Local_RDMA_Slot(send_mr.addr,Message);
   rdma_mg->Deallocate_Local_RDMA_Slot(receive_mr.addr,Message);
