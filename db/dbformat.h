4012be8ebaeca472d7f7fd24eb04ce63ebb1a7df
ruihong123 <wang4996@purdue.edu>
2023-09-24 20:19:20 -0400

    with compression, with prefix encoding

--- db/dbformat.h
+++ db/dbformat.h
@@ -30,7 +30,7 @@ static const int Immutable_FlushTrigger = 1;
 // Maximum number of unflushed immutable files 10 in 1-1,
 // 16 totally across shards in M-M (including memtable)
 // with 8 fixed shard per compute node this equals 2 or 1
-static const int Immutable_StopWritesTrigger = 31; // Default 0 shard should be 15, add memtable should be totally 16 (new 16 shards:2
+static const int Immutable_StopWritesTrigger = 15; // Default 0 shard should be 15, add memtable should be totally 16 (new 16 shards:2
 // Level-0 compaction is started when we hit this many files.
 static const int kL0_CompactionTrigger = 1;
 
@@ -38,13 +38,13 @@ static const int kL0_CompactionTrigger = 1;
 // in M-M the total number of kL0_SlowdownWritesTrigger accross shard is 24
 // in 1-1 (0 shard) this value is 16
 // with 8 fixed shard per compute node this equals 2
-static const int kL0_SlowdownWritesTrigger = 32; // (new 16 shards is 1, Default (0 shard) is 16
+static const int kL0_SlowdownWritesTrigger = 16; // (new 16 shards is 1, Default (0 shard) is 16
 
 // Maximum number of level-0 files.  We stop writes at this point.
 // in M-M the total number of kL0_SlowdownWritesTrigger accross shard is 40
 // in 1-1 (0 shard) this value is 32
 // with 8 fixed shard per compute node this equals 4
-static const int kL0_StopWritesTrigger = 64; // (new 16 shards is 2. Default(0 shard) is 32.
+static const int kL0_StopWritesTrigger = 36; // (new 16 shards is 2. Default(0 shard) is 32.
 // We  can set it as 48*64 Mega byte for the first level, then there will
 // be two levels after the random file benchmark. 1-1 256.0
 // M-M still 256. ahigher number of this value can result in low write performance
